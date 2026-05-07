0db597e3097982c5145d6abb3670d8480b694a07
joudalhallaq <156162712+joudalhallaq@users.noreply.github.com>
2026-05-07 10:28:29 +0300

    change

--- ecg/models.py
+++ ecg/models.py
@@ -4,12 +4,15 @@ from django.contrib.auth.models import User
 
 class ECGRecord(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    ecg_file = models.FileField(upload_to='ecg_files/')
+    patient_name = models.CharField(max_length=100, blank=True, null=True)
+    ecg_file = models.FileField(upload_to="ecg_files/")
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     predicted_condition = models.CharField(max_length=100, blank=True, null=True)
     confidence = models.FloatField(blank=True, null=True)
     short_explanation = models.TextField(blank=True, null=True)
+    detailed_explanation = models.TextField(blank=True, null=True)
+    xai_explanation = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f"ECG Record {self.id} - {self.user.username}"
+        return f"{self.patient_name or 'Unknown Patient'} - Record {self.id}"
