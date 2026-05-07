43d401bf189cb442e6a2c2bf4a7a3226c65a6afb
joudalhallaq <156162712+joudalhallaq@users.noreply.github.com>
2026-05-07 10:31:48 +0300

    Update models.py

--- ecg/models.py
+++ ecg/models.py
@@ -7,7 +7,7 @@ class ECGRecord(models.Model):
     patient_name = models.CharField(max_length=100, blank=True, null=True)
     ecg_file = models.FileField(upload_to="ecg_files/")
     uploaded_at = models.DateTimeField(auto_now_add=True)
-
+    patient_name = models.CharField(max_length=100, blank=True, null=True)
     predicted_condition = models.CharField(max_length=100, blank=True, null=True)
     confidence = models.FloatField(blank=True, null=True)
     short_explanation = models.TextField(blank=True, null=True)
